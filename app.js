0eb78b5f759f985527cfd62b8bce66a836fb4742
July230 <A01352823@tec.mx>
2025-03-17 14:09:48 -0600

    Agregar funcion para ventana

---
+++ app.js
@@ -0,0 +1,17 @@
+const { app, BrowserWindow} = require('electron');
+
+app.whenReady().then(() => {
+
+    // create a window
+    const myWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    // load a webpage
+    myWindow.loadFile('index.html');
+
+}).catch()
