530e67a459147e7c69d8e7bf2d90b5a68e4c285c
July230 <A01352823@tec.mx>
2025-03-17 18:44:32 -0600

    Primeros eventos

--- app.js
+++ app.js
@@ -19,3 +19,34 @@ const createWindow = () => {
 app.whenReady().then(() => {
     createWindow()
 }).catch((error) => console.log(error));
+
+/*
+On Windows and Linux, closing all windows will generally quit an application entirely. 
+To implement this pattern in your Electron app: 
+*/
+
+/*
+// there are plenty of events
+app.on('window-all-closed', () => {
+    // darwin -> MacOS
+    // win32 -> Windows
+    // linux -> Linux
+
+    // exit app if the user is not on macOS (this is for windows and linux)
+    // app will continue running on macOS
+    if (process.platform != 'darwin') {
+        app.quit() 
+    }
+})
+*/
+
+// macOS apps generally continue running even without any windows open. 
+// Because windows cannot be created before the ready event, 
+// you should only listen for activate events after your app is initialized.
+app.whenReady().then(() => {
+    createWindow()
+  
+    app.on('activate', () => {
+      if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+}).catch((error) => console.log(error));
