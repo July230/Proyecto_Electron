69bf38ef6da1d504a59ae75b6978ab0096cfa5aa
July230 <A01352823@tec.mx>
2025-03-27 11:10:28 -0600

    Refactorizar codigo

--- main.js
+++ main.js
@@ -1,24 +1,27 @@
 const { app, BrowserWindow} = require('electron');
 // app, controla el ciclo de vida de la aplicacion
 // BrowserWindow, crea y administra ventanas de la app
-const { setMainMenu } = require('./public/js/menu');
-const path = require('path')
+const { setupRoutes } = require('./routes/routes');
 
   // cargar una pagina
     // es posible crear mas de un proceso
 app.whenReady().then(() => {
-    setUpRoutes()
+    setupRoutes()
 
     // macOS apps generally continue running even without any windows open. 
     // Because windows cannot be created before the ready event, 
     // you should only listen for activate events after your app is initialized.
     app.on('activate', () => {
-        if (BrowserWindow.getAllWindows().length === 0) createWindow()
-    })
+        if (BrowserWindow.getAllWindows().length === 0){
+          setupRoutes()
+        }
+    });
 }).catch((error) => console.log(error));
 
+/*
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
       app.quit();
   }
 });
+*/
