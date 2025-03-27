feb5cc198d4616bb192b5f84701446a082694609
July230 <A01352823@tec.mx>
2025-03-27 11:22:51 -0600

    corregir main.js

--- Electron/main.js
+++ Electron/main.js
@@ -4,38 +4,36 @@ const { app, BrowserWindow} = require('electron');
 const { setupRoutes } = require('../Backend/routes/routes');
 const { setMainMenu } = require('../../Frontend/public/js/menu'); 
 
-  // cargar una pagina
-    // es posible crear mas de un proceso
-app.whenReady().then(() => {
-  // crear una ventana
-  const createMainWindow = () => {
-      const mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-          // precargar un archivo antes de cargar la app
-          preload: path.join(__dirname, 'preload.js')
-        }
-      })
-    
-      mainWindow.loadFile('../Frontend/Views/index.html')
-    
-      // al crear la ventana 
-      setMainMenu(mainWindow)
-    }
+// cargar una pagina
+// es posible crear mas de un proceso
+// crear una ventana
+const createMainWindow = () => {
+    const mainWindow = new BrowserWindow({
+      width: 800,
+      height: 600,
+      webPreferences: {
+        // precargar un archivo antes de cargar la app
+        preload: path.join(__dirname, 'preload.js')
+      }
+    })
   
-  module.exports = {
-      createMainWindow
+    mainWindow.loadFile('../Frontend/Views/index.html')
+  
+    // al crear la ventana 
+    setMainMenu(mainWindow)
 }
 
-// macOS apps generally continue running even without any windows open. 
-// Because windows cannot be created before the ready event, 
-// you should only listen for activate events after your app is initialized.
-app.on('activate', () => {
-      if (BrowserWindow.getAllWindows().length === 0){
-        setupRoutes()
-      }
-    });
+// cargar una pagina
+// es posible crear mas de un proceso
+app.whenReady().then(() => {
+  createMainWindow()
+
+    // macOS apps generally continue running even without any windows open. 
+    // Because windows cannot be created before the ready event, 
+    // you should only listen for activate events after your app is initialized.
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createMainWindow()
+    })
 }).catch((error) => console.log(error));
 
 /*
