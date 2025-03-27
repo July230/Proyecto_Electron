0271d1ac9c0894eae2c688fe0b0503545958effd
July230 <A01352823@tec.mx>
2025-03-26 21:18:19 -0600

    Mover archivos

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const { app, BrowserWindow} = require('electron');
 // app, controla el ciclo de vida de la aplicacion
 // BrowserWindow, crea y administra ventanas de la app
-const { setMainMenu } = require('./utils/js/menu');
+const { setMainMenu } = require('./public/js/menu');
 const path = require('path')
 
 console.log('Hola mundo desde electron');
